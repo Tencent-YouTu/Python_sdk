78afa8a228017eed11c233d24ec4e79ddb36d909
TencentYouTu <youtu@tencent.com>
2015-06-11 18:50:26 +0800

    modify

--- sample.py
+++ sample.py
@@ -3,26 +3,12 @@
 import time
 import TencentYoutuyun
 
-appid = '1000054'
-secret_id = 'AKIDcTZhE4OfhUOgoMzY3PiqHziE5PLziWYR'
+appid = '105054'
+secret_id = 'J9I1LNj5Wdr8TxI2VyWai3Ziz5TiZ2vZsp'
 secret_key = 'J9I1LNjWdr8TxI2VyWai3ZizTiZ2vZsp'
 
 youtu = TencentYoutuyun.YouTu(appid, secret_id, secret_key)
 
-#ret = youtu.DetectFace('0.jpg', '3041722595')
-#ret = youtu.NewPerson('jxl', '1.jpg', ['albert'])
-#faceid = 1027463655356170239
-#ret = youtu.FaceVerify('jxl', '0.jpg')
-#ret = youtu.DelPerson('jxl')
-#ret = youtu.AddFace('jxl',['0.jpg'])
-#faceid0 = 1027472526515699711
-#ret = youtu.DelFace('jxl', ['1027472526515699711'])
-#ret = youtu.SetInfo('jxl', 'jxl', 'tag')
-#ret = youtu.GetInfo('jxl')
-#ret = youtu.GetGroupIds()
-#ret = youtu.GetPersonIds('albert')
-#ret = youtu.GetFaceIds('jxl')
-#ret = youtu.GetFaceInfo('1027479018812801023')
 ret = youtu.FaceCompare('you_path_one.jpg','you_path_two.jpg')
 print ret
 
--- setup.py
+++ setup.py
@@ -5,15 +5,11 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'TencentYoutuyun',
-    version = '1.0.2',
+    version = '1.0.0',
     keywords = ('TencentYoutuyun', 'qcloud'),
-    description = 'python sdk for app.qcloud.com',
+    description = 'python sdk for open.youtu.qq.com',
     license = 'MIT License',
     install_requires=['requests'],
-
-    author = 'jayli',
-    author_email = 'jayli@tencent.com',
-    
     packages = find_packages(),
     platforms = 'any',
 )
